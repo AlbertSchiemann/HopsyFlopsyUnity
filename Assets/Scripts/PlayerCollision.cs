e8cc2152153bff7c41e621d7748f57217c45c96d
Julius <129951821+JuManecke@users.noreply.github.com>
2023-05-03 14:34:31 +0200

    CollisionDetection

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Getroffen!");
+    }
+}
