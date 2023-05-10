e6fa0f33060c4ccb93003d420cfddfd15bd2fd42
Julius <129951821+JuManecke@users.noreply.github.com>
2023-05-10 12:20:42 +0200

    FreeFall Zone reset, Level Rotation fixed, Physics still wonky

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerCollision : MonoBehaviour
 {
@@ -22,13 +23,39 @@ public class PlayerCollision : MonoBehaviour
         {
             rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
         }
+
+        if(other.gameObject.tag == "DeathZoneFreeFall")
+        {
+            Debug.Log("Free Fall Death");
+             // restart the game if the player collides with the enemy
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            
+        } 
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if(other.gameObject.tag != "Ground")
+        if(other.gameObject.tag == "Untagged")
         {
             rigidbody.constraints = RigidbodyConstraints.None;
-        }
+            Debug.Log("Exit");
+        } 
     }
+
+    private void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.tag == "Enemy")
+        {
+            Debug.Log("PlayerCollision Getroffen!");
+        }
+        
+        if(other.gameObject.tag == "Ground")
+        {
+            rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+        }
+
+    }
+
+
 }
