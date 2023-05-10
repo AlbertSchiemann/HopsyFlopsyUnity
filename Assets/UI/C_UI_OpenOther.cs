69cb8767a7c899409f8ce1bb0e01bb107f0eba68
NyraSaphira <pattybella@gmx.de>
2023-05-10 14:36:19 +0200

    Ui progress. water bar start

--- Assets/UI/C_UI_OpenOther.cs
+++ Assets/UI/C_UI_OpenOther.cs
@@ -7,22 +7,55 @@ using UnityEngine.SceneManagement;
 
 public class C_UI_OpenOther : MonoBehaviour
 {
-    public bool switcher;
-    public void ToLevel1()
-    {
-        
-          SceneManager.LoadScene("UI testing 1");
-
-    }
-
-    public void ToMainMenu()
-    {
-        SceneManager.LoadScene("UI testing");
-
+    public void ToLevel1() {        
+        SceneManager.LoadScene("UI testing 1");
+        SaveLastLevel();
     }
     public void ToLevel2()
     {
-       // SceneManager.LoadScene("UI testing");
+        // SceneManager.LoadScene("UI testing");
+        SaveLastLevel();
+    }
+    public void ToNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SaveLastLevel();
+    }
 
+    public void ToLastLevel() {
+        SceneManager.LoadScene(AlwaysThere.LastLevel);
+    }
+    public void ToRestart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ToMainMenu() {
+        SceneManager.LoadScene("UI testing");
+    }
+
+                public void ToMain_Help() 
+                {
+                    AlwaysThere.MainMenu_Index = 1;
+                    ToMainMenu();
+                }
+                public void ToMain_Settings()
+                {
+                    AlwaysThere.MainMenu_Index = 2;
+                    ToMainMenu();
+                }
+                public void ToMain_Store()
+                {
+                    AlwaysThere.MainMenu_Index = 3;
+                    ToMainMenu();
+                }
+                public void ToMain_W1()
+                {
+                    AlwaysThere.MainMenu_Index = 4;
+                    ToMainMenu();
+                }
+
+   
+    public void SaveLastLevel()  {
+        AlwaysThere.LastLevel = SceneManager.GetActiveScene().buildIndex;
     }
 }
--- Assets/UI/UI_MainMenuScript.cs
+++ Assets/UI/UI_MainMenuScript.cs
@@ -8,16 +8,31 @@ public class UI_MainMenuScript : MonoBehaviour
 {
     public static GameObject ActiveMenu;
 
+    
+
     [SerializeField] GameObject mainMenu;
     [SerializeField] GameObject helpMenu;
     [SerializeField] GameObject settingsMenu;
     [SerializeField] GameObject storeMenu;
     [SerializeField] GameObject w1Menu;
+    
+    public static GameObject MainMenu;
+    public static GameObject HelpMenu;
+    public static GameObject SettingsMenu;
+    public static GameObject StoreMenu;
+    public static GameObject W1Menu;
     // Start is called before the first frame update
     void Start()
     {
         ActiveMenu = mainMenu;
-    }
+        
+        if (AlwaysThere.MainMenu_Index == 1) OpenHelp();
+        else if (AlwaysThere.MainMenu_Index == 2) OpenSettings();
+        else if(AlwaysThere.MainMenu_Index == 3) OpenStore();
+        else if(AlwaysThere.MainMenu_Index == 4) OpenW1();
+  
+        AlwaysThere.MainMenu_Index = 0;
+}
 
  public void OpenMain()
     {
