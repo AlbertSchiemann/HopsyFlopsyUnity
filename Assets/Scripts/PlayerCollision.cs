ab4c814fdcc37f043a6dda0faaf2550dce4c4a1f
DenisFeler <106584082+DenisFeler@users.noreply.github.com>
2023-05-10 11:25:32 +0200

    Gravity Control
    made the fish fall

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    public Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Enemy")
@@ -11,5 +18,17 @@ public class PlayerCollision : MonoBehaviour
             Debug.Log("PlayerCollision Getroffen!");
         }
         
+        if(other.gameObject.tag == "Ground")
+        {
+            rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag != "Ground")
+        {
+            rigidbody.constraints = RigidbodyConstraints.None;
+        }
     }
 }
