0b7838c43731d552d1e60e03cce2c5d71096f22a
DenisFeler <106584082+DenisFeler@users.noreply.github.com>
2023-05-15 12:25:09 +0200

    Sound Manager and Folders
    added the missing folder structure related to sound and made a first iteration of a Sound Manager with it's methods.
    
    Things left to do for it to work is linking it in other scripts and adding the sound sources.

---
+++ Assets/Scripts/Audio/VolumeSlider.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeSlider : MonoBehaviour
+{
+    //[SerializeField] private Slider _slider;
+
+    void start(){
+    //    _slider.onValueChanged.AddListener(val => SoundManager.Instance.ChangeMasterVolume(val));
+    }
+}
