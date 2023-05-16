84762b318f50df01b59fe889647671fea191b79c
NyraSaphira <pattybella@gmx.de>
2023-05-16 19:22:32 +0200

    Sound buttons

--- Assets/UI/UI_MainMenuScript.cs
+++ Assets/UI/UI_MainMenuScript.cs
@@ -7,24 +7,35 @@ public class UI_MainMenuScript : MonoBehaviour
 
 {
     public static GameObject ActiveMenu;
+    public static GameObject ActiveSettings;
 
-    
 
     [SerializeField] GameObject mainMenu;
     [SerializeField] GameObject helpMenu;
     [SerializeField] GameObject settingsMenu;
     [SerializeField] GameObject storeMenu;
     [SerializeField] GameObject w1Menu;
-    
+
+    [SerializeField] GameObject settingsMain;
+    [SerializeField] GameObject settingsCredits;
+    [SerializeField] GameObject settingsSound;
+    [SerializeField] GameObject settingsContact;
+
     public static GameObject MainMenu;
     public static GameObject HelpMenu;
     public static GameObject SettingsMenu;
     public static GameObject StoreMenu;
     public static GameObject W1Menu;
+
+    public static GameObject SettingsSound;
+    public static GameObject SettingsMain;
+    public static GameObject SettingsCredits;
+    public static GameObject StoreContact;
     // Start is called before the first frame update
     void Start()
     {
         ActiveMenu = mainMenu;
+        ActiveSettings = settingsMenu;
         
         if (AlwaysThere.MainMenu_Index == 1) OpenHelp();
         else if (AlwaysThere.MainMenu_Index == 2) OpenSettings();
@@ -55,9 +66,26 @@ public class UI_MainMenuScript : MonoBehaviour
         settingsMenu.SetActive(true);
         ActiveMenu.SetActive(false);
         ActiveMenu = settingsMenu;
+        settingsSound.SetActive(false);
 
 
-    }
+    }       
+    
+                public void OpenSound()
+                {
+
+                    settingsSound.SetActive(true);
+                   // ActiveMenu.SetActive(false);
+                    ActiveSettings = settingsSound;
+                }
+
+                public void OpenSettingsMain()
+                {
+
+                    settingsMain.SetActive(true);
+                    ActiveMenu.SetActive(false);
+                    ActiveMenu = settingsMain;
+                }
     public void OpenStore()
     {
 
@@ -65,6 +93,8 @@ public class UI_MainMenuScript : MonoBehaviour
         ActiveMenu.SetActive(false);
         ActiveMenu = storeMenu;
     }
+
+ 
     public void OpenW1()
     {
 
