f42744248592c050cdcdf59d7ce539ef8d0e1991
DenisFeler <106584082+DenisFeler@users.noreply.github.com>
2023-05-17 13:58:17 +0200

    Sound Sliders
    The Sound Sliders are working in adjusting the volume of Background and Effects, Will have to reattach those in the levels as to not overwrite the progress of others.
    
    The volume gets saved, but slider value doesn't. Dunno hwhy...

--- Assets/Scripts/Audio/SoundManager.cs
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance;
+    [SerializeField] private AudioMixer mixer;
 
     [SerializeField] private AudioSource _musicSource, _effectSource, _atmoSource;
     [SerializeField] private float minPitch = .50f, maxPitch = 1.50f;
+
+    public const string Bg_key = "Background";
+    public const string Sfx_key = "Effects";
 
     //Code to only have a Single Instance of a Sound Manager alive in the Scene
     void Awake(){
@@ -22,6 +27,8 @@ public class SoundManager : MonoBehaviour
         }
         
         DontDestroyOnLoad(gameObject);
+
+        LoadVolume();
     }
 
     //Soundeffect Array, for randomized sounds in pitch and file
@@ -35,8 +42,13 @@ public class SoundManager : MonoBehaviour
         _effectSource.Play();
     }
 
-    //MasterVolume changer for a Slider
-    public void ChangeMasterVolume(float value){
-        AudioListener.volume = value;
+    public void LoadVolume() //Volume Saved in VolumeSettings.cs
+    {
+        float BgVolume = PlayerPrefs.GetFloat(Bg_key, 1f);
+        float SfxVolume = PlayerPrefs.GetFloat(Sfx_key, 1f);
+
+        mixer.SetFloat(VolumeSettings.Mixer_Bg, Mathf.Log10(BgVolume) * 20);
+        mixer.SetFloat(VolumeSettings.Mixer_Sfx, Mathf.Log10(SfxVolume) * 20);
+        Debug.Log("Volume Loaded: " + BgVolume + " " + SfxVolume);
     }
 }
