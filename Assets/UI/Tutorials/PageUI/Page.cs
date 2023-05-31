917cebe326d89c77c55401d57e94cceaf8fb8a50
NyraSaphira <pattybella@gmx.de>
2023-05-31 15:13:18 +0200

    waterAnimation + ui work

---
+++ Assets/UI/Tutorials/PageUI/Page.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource), typeof(CanvasGroup))]
+[DisallowMultipleComponent]
+
+public class Page : MonoBehaviour
+{
+
+    
+
+}
