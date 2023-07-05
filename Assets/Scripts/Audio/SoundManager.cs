1d82d3cfcba89d8cb461d347d70ab82a5a824365
DenisFeler <106584082+DenisFeler@users.noreply.github.com>
2023-07-05 10:59:41 +0200

    Sound Manager Start Stop
    Sounds don't play on game start, and stop accordingly on stopped game state

--- Assets/Scripts/Audio/SoundManager.cs
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -13,12 +13,14 @@ public class SoundManager : MonoBehaviour
     [SerializeField] private float minPitch = .50f, maxPitch = 1.50f;
 
     private bool hasPlayedSound = false;
+    private bool isAllowedToPlay = false;
+    private bool playingBg = false;
 
     public const string Bg_key = "Background";
     public const string Sfx_key = "Effects";
-
-    //Code to only have a Single Instance of a Sound Manager alive in the Scene
-    void Awake(){
+    
+    private void Awake(){
+        //Code to only have a Single Instance of a Sound Manager alive in the Scene
         if (Instance == null)
         {
             Instance = this;
@@ -28,26 +30,52 @@ public class SoundManager : MonoBehaviour
             Destroy(gameObject);
         }
         
+        //Subscription to Game State Manager
+        isAllowedToPlay = false;
+        GameStateManagerScript.onGameStart += AllowSound;
+        GameStateManagerScript.onGamePaused += DenySound;
+
         DontDestroyOnLoad(gameObject);
 
+        //Load the Volume settings, previously made by the User
         LoadVolume();
+    }
+
+    private void Update()
+    {
+        if(isAllowedToPlay && !playingBg)
+        {
+            _musicSource.Play();
+            _atmoSource.Play();
+            playingBg = true;
+        }
+        else if(!isAllowedToPlay && playingBg)
+        {
+            _musicSource.Stop();
+            _atmoSource.Stop();
+            playingBg = false;
+        }
     }
 
     //Soundeffect Array, for randomized sounds in pitch and file
     public void PlaySound(AudioClip[] clip){
-        int randomIndex = Random.Range(0, clip.Length);
-        float randomPitch = Random.Range(minPitch, maxPitch);
-        AudioClip _soundbyte = clip[randomIndex];
-
-        if (!hasPlayedSound)
+        if (isAllowedToPlay)
         {
-            _effectSource.clip = _soundbyte;
-            _effectSource.pitch = randomPitch;
-            _effectSource.Play();
-            hasPlayedSound = true;
-        }
+            int randomIndex = Random.Range(0, clip.Length);
+            float randomPitch = Random.Range(minPitch, maxPitch);
+            AudioClip _soundbyte = clip[randomIndex];
 
-        hasPlayedSound = false;
+            if (!hasPlayedSound)
+            {
+                _effectSource.clip = _soundbyte;
+                _effectSource.pitch = randomPitch;
+                _effectSource.Play();
+                hasPlayedSound = true;
+            }
+
+            hasPlayedSound = false;
+        }
+        
     }
 
     public void StopSound(AudioClip[] clip)
@@ -65,6 +93,16 @@ public class SoundManager : MonoBehaviour
 
         mixer.SetFloat(VolumeSettings.Mixer_Bg, Mathf.Log10(BgVolume) * 20);
         mixer.SetFloat(VolumeSettings.Mixer_Sfx, Mathf.Log10(SfxVolume) * 20);
-        Debug.Log("Volume Loaded: " + BgVolume + " " + SfxVolume);
+        //Debug.Log("Volume Loaded: " + BgVolume + " " + SfxVolume);
+    }
+
+    public void AllowSound()
+    {
+        isAllowedToPlay = true;
+    }
+
+    public void DenySound()
+    {
+        isAllowedToPlay = false;
     }
 }
