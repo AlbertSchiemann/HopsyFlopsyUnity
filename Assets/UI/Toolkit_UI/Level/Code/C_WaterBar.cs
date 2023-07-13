db083736f0646213332c5b1ffb6125bdbd7e6206
NyraSaphira <pattybella@gmx.de>
2023-07-13 13:08:45 +0200

    ui waterbar script! WORKING :)

---
+++ Assets/UI/Toolkit_UI/Level/Code/C_WaterBar.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class C_WaterBar : MonoBehaviour
+{
+    VisualElement waterSlider;
+    StyleLength WaterMax;
+    float water;
+
+    void OnEnable()
+    {
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+
+
+        waterSlider = root.Q<VisualElement>("Foreground");
+        SetHealth(100);
+        water = waterSlider.style.height.value.value;
+        Debug.Log("slider: " + water);
+
+    }
+
+
+
+    public void SetMaxHealth()
+    {
+        waterSlider.style.height = Length.Percent(100);
+
+    }
+
+    public void SetHealth(float health)
+    {
+      //  int value = health / water * 100;
+        waterSlider.style.height = Length.Percent(health);
+        Debug.Log("Slider: " +waterSlider.style.height);
+        //Debug.Log("health: "+health);
+    }
+
+}
