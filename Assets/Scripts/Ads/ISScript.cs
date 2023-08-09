5ea78f56e18bffff3b08eb87925fca76cb144a51
AlbertSchiemann123 <albert0schiemann@gmail.com>
2023-08-09 13:46:53 +0200

    interstitial

--- Assets/Scripts/Ads/ISScript.cs
+++ Assets/Scripts/Ads/ISScript.cs
@@ -29,6 +29,16 @@ string appkey = "1acc52165";
         IronSourceBannerEvents.onAdScreenPresentedEvent += BannerOnAdScreenPresentedEvent;
         IronSourceBannerEvents.onAdScreenDismissedEvent += BannerOnAdScreenDismissedEvent;
         IronSourceBannerEvents.onAdLeftApplicationEvent += BannerOnAdLeftApplicationEvent;
+
+        //Intestitial
+        //Add AdInfo Interstitial Events
+        IronSourceInterstitialEvents.onAdReadyEvent += InterstitialOnAdReadyEvent;
+        IronSourceInterstitialEvents.onAdLoadFailedEvent += InterstitialOnAdLoadFailed;
+        IronSourceInterstitialEvents.onAdOpenedEvent += InterstitialOnAdOpenedEvent;
+        IronSourceInterstitialEvents.onAdClickedEvent += InterstitialOnAdClickedEvent;
+        IronSourceInterstitialEvents.onAdShowSucceededEvent += InterstitialOnAdShowSucceededEvent;
+        IronSourceInterstitialEvents.onAdShowFailedEvent += InterstitialOnAdShowFailedEvent;
+        IronSourceInterstitialEvents.onAdClosedEvent += InterstitialOnAdClosedEvent;
     }
 
     void OnApplicationPause(bool isPaused)
@@ -45,6 +55,16 @@ string appkey = "1acc52165";
     public void DestroyBanner()
     {
         IronSource.Agent.destroyBanner();
+    }
+
+    public void LoadFullAd()
+    {
+        
+    }
+
+    public void ShowFullAd()
+    {
+       
     }
     private void SdkInitializationCompletedEvent() 
     {
@@ -82,6 +102,37 @@ string appkey = "1acc52165";
 
     //Full Size Callbacks
 
+    /************* Interstitial AdInfo Delegates *************/
+    // Invoked when the interstitial ad was loaded succesfully.
+    void InterstitialOnAdReadyEvent(IronSourceAdInfo adInfo)
+    {
+    }
+    // Invoked when the initialization process has failed.
+    void InterstitialOnAdLoadFailed(IronSourceError ironSourceError)
+    {
+    }
+    // Invoked when the Interstitial Ad Unit has opened. This is the impression indication. 
+    void InterstitialOnAdOpenedEvent(IronSourceAdInfo adInfo)
+    {
+    }
+    // Invoked when end user clicked on the interstitial ad
+    void InterstitialOnAdClickedEvent(IronSourceAdInfo adInfo)
+    {
+    }
+    // Invoked when the ad failed to show.
+    void InterstitialOnAdShowFailedEvent(IronSourceError ironSourceError, IronSourceAdInfo adInfo)
+    {
+    }
+    // Invoked when the interstitial ad closed and the user went back to the application screen.
+    void InterstitialOnAdClosedEvent(IronSourceAdInfo adInfo)
+    {
+    }
+    // Invoked before the interstitial ad was opened, and before the InterstitialOnAdOpenedEvent is reported.
+    // This callback is not supported by all networks, and we recommend using it only if  
+    // it's supported by all networks you included in your build. 
+    void InterstitialOnAdShowSucceededEvent(IronSourceAdInfo adInfo)
+    {
+    }
 
     //Rewarded Callbacks
 }
